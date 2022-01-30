9e93135065a671c21ecff374f718cfa114811c54
Abdul ムッタキン <abdulmuttaqin456@gmail.com>
2022-01-31 04:07:26 +0700

    Update getData.js

--- utils/getData.js
+++ utils/getData.js
@@ -11,7 +11,7 @@ let country;
 for (var i =0; i < list.length; i++){
 let code = list[i].split("=")[1];
 let name = list[i].split("=")[0].replace("_", " ");
-console.log(list[i]);
+
 const url = 'https://trends.google.com/trends/hottrends/atom/feed?pn='+ code;
   const feed = await parser.parseURL(url);
   const x = feed.items.map(el => el.title);
@@ -31,6 +31,7 @@ country += `
      fs.writeFileSync("./data/"+name+".json", JSON.stringify(res, null, 2), res)
     
 }
+console.log(country);
 fs.writeFileSync("./README.MD", `
 
 ## Google Trends Keywords Scraper 
