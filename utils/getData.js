6fda5ee44c92745705985eecebec399fba6484ec
Abdul ムッタキン <abdulmuttaqin456@gmail.com>
2022-01-31 07:46:04 +0700

    Update getData.js

--- utils/getData.js
+++ utils/getData.js
@@ -22,7 +22,7 @@ const url = 'https://trends.google.com/trends/hottrends/atom/feed?pn='+ code;
 
 console.log(x.toString());
 str += `
-| ${name} | ${x.toString().replace(\/\n/g , "")}|
+| ${name} | ${x.toString().replace(/\n|\r/g,"")}|
 
 `
      fs.writeFileSync("./data/"+name+".json", JSON.stringify(res, null, 2), res)
