65f9c4a9f5a3d1c4f9ff4b137e36d8bb1ea94276
Abdul ムッタキン <abdulmuttaqin456@gmail.com>
2022-01-31 07:48:59 +0700

    Update getData.js

--- utils/getData.js
+++ utils/getData.js
@@ -21,9 +21,7 @@ const url = 'https://trends.google.com/trends/hottrends/atom/feed?pn='+ code;
 }
 
 console.log(x.toString());
-str += `
-| ${name} | ${x.toString().replace(/\n|\r/g,"")}|
-`
+str += "| "+name+" | "+x.toString().replace(/\n|\r/g,"")+"|";
      fs.writeFileSync("./data/"+name+".json", JSON.stringify(res, null, 2), res)
     
 }
@@ -38,7 +36,7 @@ Country List :
  Name  | Keyword |
 | ------------- | ------------- |
 ${str}
- 
+
 
 © Abdul Muttaqin 
 `)
