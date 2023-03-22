c4f9a7218264b7d0593dc6ea8bb59a80ea261026
BAGUS BUDI SATOTO <87259393+bagussatoto@users.noreply.github.com>
2023-03-23 01:05:16 +0700

    Update ``repo new``

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const getData = require("./utils/getData")
+const getimport = Request("./data/get data")
 const fs = require("fs");
 var list = fs.readFileSync("id.txt",'utf8');
 let data = list.split("\n");
