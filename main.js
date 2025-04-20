94a873a22727428e17d58239c185dddbf253c5c5
taqin <abdulmuttaqin456@gmail.com>
2025-04-21 05:44:37 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
-const getData = require("./utils/getData")
+const getData = require("./utils/getData");
 const fs = require("fs");
-var list = fs.readFileSync("id.txt",'utf8');
-let data = list.split("\n");
-console.log(data);
-getData(data);
+
+async function main() {
+  try {
+    if (!fs.existsSync("./data")) {
+      fs.mkdirSync("./data");
+    }
+    
+    if (!fs.existsSync("./forcopied")) {
+      fs.mkdirSync("./forcopied");
+    }
+    
+    const list = fs.readFileSync("id.txt", 'utf8');
+    const data = list.split("\n").filter(line => line.trim() !== '');
+    
+    console.log(`Processing ${data.length} country codes...`);
+    await getData(data);
+    console.log("Process completed successfully!");
+  } catch (error) {
+    console.error("Error in main process:", error);
+  }
+}
+
+main();
